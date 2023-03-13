266ac86ce0fdddbdef3fd2cdde00641fa1d75fb0
Bastien Silhol <bastien.silhol@epitech.eu>
2023-03-13 19:12:47 +0100

    added local scripts

--- commons/abis.js
+++ commons/abis.js
@@ -1,14 +1,14 @@
-const HOME_ERC_TO_NATIVE_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/erc20_to_native/HomeBridgeErcToNative.sol/HomeBridgeErcToNative.json').abi
-const FOREIGN_ERC_TO_NATIVE_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/erc20_to_native/XDaiForeignBridge.sol/XDaiForeignBridge.json').abi
-const ERC20_ABI = require('../contracts/artifacts/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol/ERC20.json').abi
-const BLOCK_REWARD_ABI = require('../contracts/artifacts/contracts/mocks/BlockRewardMock.sol/BlockRewardMock.json').abi
-const BRIDGE_VALIDATORS_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/BridgeValidators.sol/BridgeValidators.json').abi
-const REWARDABLE_VALIDATORS_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/RewardableValidators.sol/RewardableValidators.json').abi
-const HOME_AMB_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/arbitrary_message/HomeAMB.sol/HomeAMB.json').abi
-const FOREIGN_AMB_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/arbitrary_message/ForeignAMB.sol/ForeignAMB.json').abi
-const BOX_ABI = require('../contracts/artifacts/contracts/mocks/Box.sol/Box.json').abi
-const HOME_AMB_ERC_TO_ERC_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/amb_erc677_to_erc677/HomeAMBErc677ToErc677.sol/HomeAMBErc677ToErc677.json').abi
-const FOREIGN_AMB_ERC_TO_ERC_ABI = require('../contracts/artifacts/contracts/upgradeable_contracts/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol/ForeignAMBErc677ToErc677.json').abi
+const HOME_ERC_TO_NATIVE_ABI = require('../contracts/build/contracts/HomeBridgeErcToNative.json').abi
+const FOREIGN_ERC_TO_NATIVE_ABI = require('../contracts/build/contracts/XDaiForeignBridge.json').abi
+const ERC20_ABI = require('../contracts/build/contracts/ERC20.json').abi
+const BLOCK_REWARD_ABI = require('../contracts/build/contracts/BlockRewardMock.json').abi
+const BRIDGE_VALIDATORS_ABI = require('../contracts/build/contracts/BridgeValidators.json').abi
+const REWARDABLE_VALIDATORS_ABI = require('../contracts/build/contracts/RewardableValidators.json').abi
+const HOME_AMB_ABI = require('../contracts/build/contracts/HomeAMB.json').abi
+const FOREIGN_AMB_ABI = require('../contracts/build/contracts/ForeignAMB.json').abi
+const BOX_ABI = require('../contracts/build/contracts/Box.json').abi
+const HOME_AMB_ERC_TO_ERC_ABI = require('../contracts/build/contracts/HomeAMBErc677ToErc677.json').abi
+const FOREIGN_AMB_ERC_TO_ERC_ABI = require('../contracts/build/contracts/ForeignAMBErc677ToErc677.json').abi
 
 const { BRIDGE_MODES } = require('./constants')
 
---
+++ oracle/startBridge.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+yarn watcher:signature-request & pids+=" $!"
+yarn watcher:collected-signatures & pids+=" $!"
+yarn watcher:affirmation-request & pids+=" $!"
+yarn sender:home & pids+=" $!"
+yarn sender:foreign & pids+=" $!"
+
+trap "kill $pids" SIGTERM SIGINT
+wait $pids
+
+echo "Oracle StartBridge DONE"
