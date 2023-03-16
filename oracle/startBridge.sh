7118c8bdebf3898c7bed101770cb416750a89c3b
Bastien Silhol <bastien.silhol@epitech.eu>
2023-03-16 11:40:19 +0100

    update scripts

--- oracle/startBridge.sh
+++ oracle/startBridge.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+if [[ $* == *--no-deploy* ]]
+then
+  echo "No deploy..."
+else
+  pwd=`pwd`
+
+  cd ../contracts/deploy
+  yarn deploy
+
+  cd $pwd
+fi
+
+brew services restart rabbitmq & pids=$!
+redis-server & pids+=" $!"
+
+echo "Sleeping 20s ..."
+
+sleep 20
+
+echo "done - starting process"
+
 yarn watcher:signature-request & pids+=" $!"
 yarn watcher:collected-signatures & pids+=" $!"
 yarn watcher:affirmation-request & pids+=" $!"
